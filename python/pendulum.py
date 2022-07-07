32353c40a23d0e1da4f812a4f4f256970017e71b
tr3nt0n <82298389+EugenioBarbieriViale@users.noreply.github.com>
2022-07-07 12:57:09 +0200

    make pendulum oop

--- python/pendulum.py
+++ python/pendulum.py
@@ -1,44 +1,16 @@
-# Sources:
-# https://www.myphysicslab.com/pendulum/pendulum-en.html
-# https://www.youtube.com/watch?v=NBWMtlbbOag
-# http://calculuslab.deltacollege.edu/ODE/7-A-2/7-A-2-h.html
-
-import pygame, sys
 from numpy import sqrt, cos, arcsin, pi, sin
 
-pygame.init()
-clock = pygame.time.Clock()
+class Pendulum:
+    def __init__(self, length, angle, gravity):
+        self.length = length
+        self.angle = angle
+        self.gravity = gravity
+        self.angleV = 0
+        self.angleA = 0
 
-screenx,screeny = 1000,800
-
-screen = pygame.display.set_mode([screenx,screeny])
-pygame.display.set_caption("Pendulum")
-
-length = 300
-angle = 0.7 # radians
-angleV = 0 # angular velocity
-angleA = 0 # angular acceleration
-gravity = 1
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    screen.fill((0,0,0))
-
-    x = length*sin(angle)+500
-    y = length*cos(angle)+50
-    force = sin(angle)*gravity
-
-    angleA = (force*-1)/length
-    angleV += angleA
-    angle += angleV
-
-    pygame.draw.line(screen, (255,255,255),(500,50), (x,y), width=3)
-    pygame.draw.circle(screen, (0,0,255), (x,y), 30)
-
-    pygame.display.flip()
-    clock.tick(60)
-    pygame.display.update()
+    def move(self):
+        self.x = self.length*sin(self.angle)+500
+        self.y = self.length*cos(self.angle)+50
+        self.angleA = -(sin(self.angle)*self.gravity)/self.length
+        self.angleV += self.angleA
+        self.angle += self.angleV
